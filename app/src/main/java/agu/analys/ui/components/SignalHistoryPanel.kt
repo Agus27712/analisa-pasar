13e6106bb0eb87cec08cfac5381000984aa12f36
ak27712 <pvszombie@gmail.com>
2026-08-13 19:07:10 +0700

    Update SignalHistoryPanel.kt
    import androidx.compose.ui.draw.clip 
    from 
    import androidx.compose.ui.clip

--- app/src/main/java/agu/analys/ui/components/SignalHistoryPanel.kt
+++ app/src/main/java/agu/analys/ui/components/SignalHistoryPanel.kt
@@ -26,8 +26,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
