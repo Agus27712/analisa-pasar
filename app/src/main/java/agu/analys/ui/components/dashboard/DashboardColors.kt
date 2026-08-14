0d54200217d38f923356029ac16ca2ab39ba895e
ak27712 <pvszombie@gmail.com>
2026-08-15 02:17:34 +0700

    refactor(ui): shared dashboard color tokens

---
+++ app/src/main/java/agu/analys/ui/components/dashboard/DashboardColors.kt
@@ -0,0 +1,14 @@
+package agu.analys.ui.components.dashboard
+
+import androidx.compose.ui.graphics.Color
+
+object DashboardColors {
+    val Background = Color(0xFF070A0F)
+    val Surface = Color(0xFF0D1722)
+    val Card = Color(0xFF0D1722)
+    val Border = Color(0xFF1A3347)
+    val AccentBlue = Color(0xFF2196F3)
+    val AccentBlueSoft = Color(0xFF6FB8FF)
+    val Gold = Color(0xFFFFD54A)
+    val Amber = Color(0xFFFFB300)
+}
