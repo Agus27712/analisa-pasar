f13d7a5ca138c1da0240c8495f8be0a4588dc57b
ak27712 <pvszombie@gmail.com>
2026-08-15 19:02:32 +0700

    Fix Compose animation transition import

--- app/src/main/java/agu/analys/ui/animation/AppAnimations.kt
+++ app/src/main/java/agu/analys/ui/animation/AppAnimations.kt
@@ -14,6 +14,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
