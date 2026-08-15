2ce7f5de6841d12d54331fcf6fd25a2f67368c65
ak27712 <pvszombie@gmail.com>
2026-08-15 19:55:26 +0700

    chore: set next APK release version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     minSdk = 24
     targetSdk = 35
     versionCode = providers.gradleProperty("VERSION_CODE").map(String::toInt).getOrElse(6)
-    versionName = "1.1.7"
+    versionName = "1.1.8"
   }
 
   signingConfigs {
